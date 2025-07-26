5fe354878db40603f795170741f1afcbf908fc66
aashishkewl01 <aashish@tummee.com>
2025-07-26 13:39:04 +0530

    (Aashish) Replaced the complete frontend with React for more flexibility
    Setup Docker for both frontend and backend instead of running manually + auto refresh enabled

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="min-h-screen text-white bg-black flex items-center justify-center">
+      <h1 className="text-3xl">King Shit</h1>
+    </div>
+  );
+}
+
+export default App;
